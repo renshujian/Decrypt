b4ea91fe1df66fa31985e5a1512283582314b580
James Ren <renshujian0@hotmail.com>
2021-07-12 21:31:38 +0800

    添加标签解码功能, 3h

--- Decrypt/MainWindow.xaml.cs
+++ Decrypt/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 using Microsoft.Extensions.Configuration;
 
 namespace Decrypt
@@ -8,11 +9,30 @@ namespace Decrypt
     /// </summary>
     public partial class MainWindow : Window
     {
-        IConfigurationRoot config = new ConfigurationBuilder().AddIniFile("config.ini").Build();
+        static readonly IConfigurationRoot config = new ConfigurationBuilder().AddIniFile("config.ini").Build();
+        readonly DecryptService decryptService = new(config.GetSection(nameof(DecryptService))[nameof(DecryptService.Endpoint)]);
+        readonly DispatcherTimer decryptDelay = new();
 
         public MainWindow()
         {
             InitializeComponent();
+            input.Focus();
+            decryptDelay.Interval = System.TimeSpan.FromSeconds(0.3);
+            decryptDelay.Tick += async (o, e) =>
+            {
+                decryptDelay.Stop();
+                partID.Text = await decryptService.Decrypt(input.Text);
+                input.Text = "";
+            };
+        }
+
+        private void input_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (input.Text != "")
+            {
+                decryptDelay.Stop();
+                decryptDelay.Start();
+            }
         }
     }
 }
