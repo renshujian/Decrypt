24895cca9910acc8905ba263f61a5eccf56401da
James Ren <renshujian0@hotmail.com>
2021-07-12 22:28:18 +0800

    添加curve info处理, 8h

--- Decrypt/MainWindow.xaml.cs
+++ Decrypt/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Threading;
 using Microsoft.Extensions.Configuration;
 
@@ -12,6 +16,7 @@ namespace Decrypt
         static readonly IConfigurationRoot config = new ConfigurationBuilder().AddIniFile("config.ini").Build();
         readonly DecryptService decryptService = new(config.GetSection(nameof(DecryptService))[nameof(DecryptService.Endpoint)]);
         readonly DispatcherTimer decryptDelay = new();
+        readonly FileSystemWatcher fileSystemWatcher = new(config.GetSection("FileWatcher")["Directory"], "*.csv");
 
         public MainWindow()
         {
@@ -24,6 +29,15 @@ namespace Decrypt
                 partID.Text = await decryptService.Decrypt(input.Text);
                 input.Text = "";
             };
+            fileSystemWatcher.Created += async (o, e) =>
+            {
+                if (File.Exists(e.FullPath))
+                {
+                    await Task.Delay(300);
+                    await Dispatcher.InvokeAsync(() => ProcessFile(e.FullPath, partID.Text));
+                }
+            };
+            fileSystemWatcher.EnableRaisingEvents = true;
         }
 
         private void input_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
@@ -33,6 +47,24 @@ namespace Decrypt
                 decryptDelay.Stop();
                 decryptDelay.Start();
             }
+        }
+
+        static void ProcessFile(string path, string newPartID)
+        {
+            var text = File.ReadAllText(path);
+            var match = Regex.Match(text, @"\bCurve Info.+?\n\n\n", RegexOptions.Singleline);
+            var info = match.Success ? match.Value : throw new ApplicationException("csv中未找到Curve Info，请确认文件格式");
+            var lines = info.Split('\n');
+            match = Regex.Match(lines[1], @"(\w+?,)*partID");
+            var columnIndex = match.Success ? match.Groups[1].Captures.Count : throw new ApplicationException("Curve Info中未找到partID，请确认文件格式");
+            var columns = lines[2].Split(',');
+            var partID = columns[columnIndex].Trim('"');
+            columns[columnIndex] = '"' + newPartID + '"';
+            lines[2] = string.Join(',', columns);
+            var newInfo = string.Join('\n', lines);
+            File.WriteAllText(path, text.Replace(info, newInfo));
+            newPartID = newPartID.Replace('/', '_').Replace('*', '_');
+            File.Move(path, Path.GetFullPath(path.Replace(partID, newPartID)));
         }
     }
 }
